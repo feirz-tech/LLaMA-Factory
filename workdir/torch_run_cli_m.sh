f0d58e24a025e81a392d8e845407e597390593e5
feirz-tech <feir.z@binance.com>
2025-10-13 22:30:41 +0800

    refine: train script multi node

---
+++ workdir/torch_run_cli_m.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NNODES=$2
+NODE_RANK=$RANK
+NPROC_PER_NODE=8
+
+# 获取脚本所在目录
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# LLaMA-Factory 根目录是脚本目录的上一级
+ROOT="$(dirname "$SCRIPT_DIR")"
+
+export PYTHONPATH=$ROOT/src:$ROOT:$PYTHONPYTH
+
+FORCE_TORCHRUN=1
+CONFIG=$1
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+CONFIG_NAME=$(basename "$CONFIG" "${CONFIG##*.}")
+
+mkdir -p logs
+
+python -m llamafactory.cli train $CONFIG 2>&1 | tee logs/run_${CONFIG_NAME}_${TIMESTAMP}.log
